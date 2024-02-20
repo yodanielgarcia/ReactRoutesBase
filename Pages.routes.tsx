3f7b34a21814053b36298fc24e96c71490804ae5
yodanielgarcia <31499858+yodanielgarcia@users.noreply.github.com>
2024-02-20 15:24:44 -0500

    Create Pages.routes.tsx

---
+++ Pages.routes.tsx
@@ -0,0 +1,54 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+import Routes from 'constants/routes';
+import Hierarchy from 'pages/Hierarchy/Hierarchy';
+import Edit from './Edit';
+import { useGetUserRolesQuery } from 'api/hierarchy/getUserRoles';
+import Loader from 'components/shared/Loader';
+import { Typography } from '@portal-platform/ui-components';
+import * as Strings from 'constants/strings';
+import ProtectedRoute, { StateRoute } from 'components/ProtectedRoute';
+import { GetHierarchyNodeInformationParameters } from 'api/hierarchy';
+import { LocationState } from 'types/LocationState';
+
+const PagesRoutes = () => {
+  const { isError, isFetching } = useGetUserRolesQuery();
+
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <Navigate replace to={Routes.base} />,
+    },
+    {
+      path: `${Routes.base}`,
+      element: <Hierarchy />,
+    },
+    {
+      path: `${Routes.base}${Routes.edit}`,
+      element: (
+        <ProtectedRoute grant="canUpdateHierarchy">
+          <StateRoute<LocationState<GetHierarchyNodeInformationParameters>>
+            verifyRouterState={['from', 'hierarchy', 'nodeId']}
+          >
+            <Edit />
+          </StateRoute>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '*',
+      element: <Navigate replace to={Routes.base} />,
+    },
+  ]);
+
+  if (isFetching) return <Loader />;
+  if (isError)
+    return (
+      <Typography variant="h6" align="center">
+        {Strings.errorValidatingUserRoles()}
+      </Typography>
+    );
+
+  return routes;
+};
+
+export default PagesRoutes;
