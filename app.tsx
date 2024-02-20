fa5c10e5c18606927896807d2b17e8f2e6d62bac
yodanielgarcia <31499858+yodanielgarcia@users.noreply.github.com>
2024-02-20 15:36:35 -0500

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -4,12 +4,14 @@ import { createGenerateClassName } from '@material-ui/styles';
 import ErrorBoundary from 'components/shared/ErrorBoundary';
 import AppError from './AppError';
 import index from 'pagesIndex';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 export const App: FC<IProps> = ({ authToken, theme }) => {
   return (
     <ErrorBoundary FallbackComponent={AppError}>
       <div style={{ padding: 20 }}>
+        <Router>
             <Pages />
+        </Router>
       </div>
     </ErrorBoundary>
   );
